b2af234bfb077442855f80b66938fe9c834b77de
Arti Borad <dev47.scriptus@gmail.com>
2025-02-19 17:33:02 +0530

    - create upload folder dynamically

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthMiddleware } from './auth/auth.middleware';
+import { join } from 'path';
+import { existsSync, mkdirSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const uploadFolder = join(__dirname, '..', 'uploads');
+  if (!existsSync(uploadFolder)) {
+    mkdirSync(uploadFolder);
+    console.log('Upload folder created.');
+  }
   app.use(new AuthMiddleware().use);
   await app.listen(3000);
 }
