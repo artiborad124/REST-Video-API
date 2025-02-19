eebee67f2682f2e71709f3b1ea15946704015181
Arti Borad <dev47.scriptus@gmail.com>
2025-02-19 17:18:00 +0530

    - initialize project with NestJS and TypeScript - setup SQLite database and configure TypeORM

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideoModule } from './video/video.module';
+import { Video } from './video/entity/video.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'video.sqlite',
+      entities: [Video],
+      synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), // points to the uploads directory
+      serveRoot: '/uploads', // base URL for static assets
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
+    VideoModule,
+    MulterModule.register({
+      storage: multer.memoryStorage(),
+      limits:{
+        fieldSize : 25 * 1024 * 1024,
+      }
+    }),
+  ],
+})
+export class AppModule {}
